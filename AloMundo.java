688fcf852d65da83b7f31c31b443550dd117050f
Liniker <liniker50@hotmail.com>
2018-08-01 12:41:55 -0300

    mais alterações

--- AloMundo.java
+++ AloMundo.java
@@ -2,6 +2,8 @@ public class AloMundo{
 	public static void main(String[] args){
 	System.out.println("Alo mundo");
 	System.out.println("Seja bem-vindo!");
+    System.out.println("Tio Sam!");
+
 		
 	}
 
